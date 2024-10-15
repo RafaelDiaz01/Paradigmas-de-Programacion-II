852a30c1e3398783c6814cb86fb313f364fce116
Rafael Díaz desde Laptop <rafaeldiazmx98@gmail.com>
2024-10-15 00:05:35 -0600

    Se actualizó el archivo para agregar los comentarios necesarios.

--- Python/Fundamentos-Python2/Tipo_de_datos_ejercicio.py
+++ Python/Fundamentos-Python2/Tipo_de_datos_ejercicio.py
@@ -3,32 +3,32 @@
 
 presupuesto = 180
 
-# Datos para el lunes
-dia_lunes = "lunes"
-pasaje_lunes = 20
-comida_lunes = 135.5
-otros_gastos_lunes = 34.5
+#Datos para el lunes
+dia_lunes = "lunes" #Cadena de texto
+pasaje_lunes = 20 #Número entero
+comida_lunes = 135.5 #Número decimal
+otros_gastos_lunes = 34.5 #Número decimal
 total_gastos_lunes = pasaje_lunes + comida_lunes + otros_gastos_lunes
 mayor_presupuesto_lunes = total_gastos_lunes > presupuesto
 
-# Mostrar los gastos del lunes
+#Mostrar los gastos del lunes
 print("*** Gastos diarios ***")
 print(f"Día: {dia_lunes}")
 print(f"Pasaje: {pasaje_lunes}")
 print(f"Comida: {comida_lunes}")
 print(f"Otros gastos: {otros_gastos_lunes}")
 print(f"¿Fue mayor a mi presupuesto?: {mayor_presupuesto_lunes}")
-print()  # Línea en blanco para separar días
+print()  #Salto de línea
 
-# Datos para el martes
-dia_martes = "martes"
-pasaje_martes = 12
-comida_martes = 45.5
-otros_gastos_martes = 4.5
+#Datos para el martes
+dia_martes = "martes" #Cadena de texto
+pasaje_martes = 12 #Número entero
+comida_martes = 45.5 #Número decimal
+otros_gastos_martes = 4.5 #Número decimal
 total_gastos_martes = pasaje_martes + comida_martes + otros_gastos_martes
 mayor_presupuesto_martes = total_gastos_martes > presupuesto
 
-# Mostrar los gastos del martes
+#Mostrar los gastos del martes
 print("*** Gastos diarios ***")
 print(f"Día: {dia_martes}")
 print(f"Pasaje: {pasaje_martes}")
