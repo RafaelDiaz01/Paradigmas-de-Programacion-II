10aa29f7ecedf83f5af662d0c80a8281eba11822
Rafael Díaz desde Laptop <rafaeldiazmx98@gmail.com>
2024-10-24 10:09:28 -0600

    Se realizaron los ejercicios y se hicieron comentarios adecuados en cada linea de código.

--- Python/Sentencias-de-control/Sentencias_ejercicio1.py
+++ Python/Sentencias-de-control/Sentencias_ejercicio1.py
@@ -1,9 +1,14 @@
 #Kevin Rafael Díaz López - 23/10/2024
-#Programa que determina un grupo de edad.
+#Programa que determina que número es menor.
 
 print("\n\t**PROGRAMA QUE CUAL DE LOS 2 NÚMEROS ES MENOR**")
 
 numero1 = int((input("Ingrese un número: ")))
 numero2 = int((input("Ingrese otro número: ")))
 
-print(f"El numero es menor que")
+if numero1 < numero2 :
+    print(f"El número {numero1} es menor que el número {numero2}")
+elif numero2 < numero1:
+    print(f"El número {numero2} es menor que el número {numero1}")
+else:
+    print("Los números son iguales")
--- Python/Sentencias-de-control/Sentencias_ejercicio2.py
+++ Python/Sentencias-de-control/Sentencias_ejercicio2.py
@@ -1,11 +1,22 @@
 #Kevin Rafael Díaz López - 23/10/2024
-#Programa que determina un grupo de edad.
+#Programa que determina la estación del año dependiendo del mes.
 
 print("\n\t**PROGRAMA QUE DETERMINA LA ESTACIÓN DEL AÑO**")
 
 mes = int((input("Ingrese el número de mes: ")))
 
-print(f"La estación es: {mes}")
+if mes <= 0:
+    print("Error, esa estación no existe")
+elif mes < 3 or mes == 12:
+    print("La estación es invierno")
+elif mes < 6:
+    print("La estación es primavera")
+elif mes < 9:
+    print("La estación es verano")
+elif mes < 12:
+    print("La estación es otoño")
+else:
+    print("Incorrect")
 
 #otro:incorrect
 #3,4,5 primavera
