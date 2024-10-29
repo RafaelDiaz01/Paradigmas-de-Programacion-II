67494d0576bc3a0d4ea90790bb291c51258df397
Rafael Díaz desde Laptop <rafaeldiazmx98@gmail.com>
2024-10-29 15:19:50 -0600

    Se realizaron los ejercicios correspondientes, agregando los comentarios adecuados en cada linea de código.

--- Python/Sentencias-de-control/Sentencias_ejercicio5.py
+++ Python/Sentencias-de-control/Sentencias_ejercicio5.py
@@ -9,16 +9,16 @@ RESET = "\033[0m"   # Define el color de reinicio para volver al color original.
 print(f"\n\t{GREEN}|PROGRAMA PARA CALCULAR EL PROMEDIO DE UNA MATERIA|{RESET}")  # Imprime el título del programa en color verde.
 print()  # Imprime una línea en blanco.
 
-parcial1 = float(input("Ingrese la calificación del primer parcial: "))
-parcial2 = float(input("Ingrese la calificación del segundo parcial: "))
-parcial3 = float(input("Ingrese la calificación del tercer parcial: "))
-ordinario = float(input("Ingrese la calificación del ordinario: "))
+parcial1 = float(input("Ingrese la calificación del primer parcial: ")) # Solicita la calificación del primer parcial.
+parcial2 = float(input("Ingrese la calificación del segundo parcial: ")) # Solicita la calificación del segundo parcial.
+parcial3 = float(input("Ingrese la calificación del tercer parcial: ")) # Solicita la calificación del tercer parcial.
+ordinario = float(input("Ingrese la calificación del ordinario: ")) # Solicita la calificación del ordinario.
 
-calificacion = (((parcial1 + parcial2 + parcial3) / 3) + ordinario) / 2
+calificacion = (((parcial1 + parcial2 + parcial3) / 3) + ordinario) / 2 # Calcula el promedio de las calificaciones ingresadas.
 
-if calificacion < 0 or calificacion > 10:
+if calificacion < 0 or calificacion > 10: # Si el promedio es menor a 0 o mayor a 10 imprime un mensaje de error.
     print("E R R O R")
-elif calificacion <6:
+elif calificacion <6: # Si el promedio es menor a 6 pero mayor a 0 imprime un mensaje de reprobado.
     print(f"La calificación final es de: {calificacion:.1f}. Lo siento, no aprobaste.")
-else:
+else: # Si no se cumplieron las condiciones anteriores, significa que el alumno aprobó.
     print(f"La calificación final es de: {calificacion:.1f}. ¡Felicidades! Aprobaste")
