218ba9ec3e2a54c1afd1c06b0f6c85f5e0dc3d92
Rafael Díaz desde Laptop <rafaeldiazmx98@gmail.com>
2024-11-03 20:56:01 -0600

    Se realizaron los ejercicios correspondientes, agregando los comentarios adecuados en cada linea de código.

---
+++ Python/Examen-Parcial-1/Ejercicio1_lic_informatica.py
@@ -0,0 +1,10 @@
+# Kevin Rafael Díaz López - 28/10/2024
+# Programa que imprime en consola los números, separados por comas, del 1 hasta un número ingresado por el usuario.
+
+# Códigos para dar color al título.
+GREEN = "\033[32m"  # Define el color verde para el texto.
+RESET = "\033[0m"  # Define el color de reinicio para volver al color original.
+
+# Título del programa.
+print(f"\n\t{GREEN}|LICENCIATURA EN INFORMÁRTICA|{RESET}")  # Imprime el título del programa en color verde.
+print()  # Imprime una línea en blanco.
---
+++ Python/Examen-Parcial-1/README.md
@@ -0,0 +1,3 @@
+Directorio para subir los ejercicios del examen correspondiente al primer parcial.
+
+1. Ejercicio1_lic_informatica
