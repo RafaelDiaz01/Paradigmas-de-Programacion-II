da290d5d07822dabed628969b1df60708dec60a2
Rafael Díaz desde Laptop <rafaeldiazmx98@gmail.com>
2024-11-03 21:17:20 -0600

    Se realizaron los ejercicios correspondientes al examen del primer parcial, agregando los comentarios adecuados en cada linea de código.

---
+++ Python/Examen-Parcial-1/Ejercicio3_piedra_papel_tijeras.py
@@ -0,0 +1,10 @@
+# Kevin Rafael Díaz López - 28/10/2024
+# Programa que simula el famoso juego de "piedra, papel o tijera", en donde el contrincante es la CPU. La opción de la CPU se escogerá de forma aleatorio con la función randint().
+
+# Códigos para dar color al título.
+GREEN = "\033[32m"  # Define el color verde para el texto.
+RESET = "\033[0m"  # Define el color de reinicio para volver al color original.
+
+# Título del programa.
+print(f"\n\t{GREEN}|JUEGO DE PIEDRA, PAPEL O TIJERA|{RESET}")  # Imprime el título del programa en color verde.
+print()  # Imprime una línea en blanco.
