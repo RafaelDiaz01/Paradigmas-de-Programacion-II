76412189370f5721897bf7f544f64e9eb2d340ce
Rafael Díaz desde Laptop <rafaeldiazmx98@gmail.com>
2024-11-03 21:01:30 -0600

    Se realizaron los ejercicios correspondientes al examen del primer parcial, agregando los comentarios adecuados en cada linea de código.

---
+++ Python/Examen-Parcial-1/Ejercicio2_area_perimetro.py
@@ -0,0 +1,10 @@
+# Kevin Rafael Díaz López - 28/10/2024
+# Programa que determina el área y el perímetro de un rectángulo o de un círculo.
+
+# Códigos para dar color al título.
+GREEN = "\033[32m"  # Define el color verde para el texto.
+RESET = "\033[0m"  # Define el color de reinicio para volver al color original.
+
+# Título del programa.
+print(f"\n\t{GREEN}|PROGRAMA QUE CALCULA EL ÁREA Y EL PERÍMETRO|{RESET}")  # Imprime el título del programa en color verde.
+print()  # Imprime una línea en blanco.
