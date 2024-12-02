a1408db4e7fd9a51ecb6d2a04c712888ec706d99
Rafael Díaz desde Laptop <rafaeldiazmx98@gmail.com>
2024-12-02 10:36:02 -0600

    Código comentado.

--- Python/Ciclos/Ciclos_ejercicio5_piramide.py
+++ Python/Ciclos/Ciclos_ejercicio5_piramide.py
@@ -1,38 +1,39 @@
 # Kevin Rafael Díaz López - 07/11/2024
-# Programa que imprime una piramide.
+# Programa que imprime una pirámide mediante el uso de ciclos.
 
 # Códigos para dar color al título.
 GREEN = "\033[32m"  # Define el color verde para el texto.
 RESET = "\033[0m"  # Define el color de reinicio para volver al color original.
 
 # Título del programa.
-print(f"\n\t{GREEN}|PIRAMIDE|{RESET}")  # Imprime el título del programa en color verde.
+print(f"\n\t{GREEN}|PIRÁMIDE|{RESET}")  # Imprime el título del programa en color verde.
 print()  # Imprime una línea en blanco.
 
 filas = int(input("Ingrese el número de filas: "))
 print() # Imprime un salto de línea.
+# Se definen las variables para el carácter del asterisco y el espacio.
 asterisco = "*"
 espacio = " "
 
-print("PIRAMIDE 1")
+print("PIRÁMIDE 1")
 for i in range(filas+1):
     espacios = espacio * i
     asteriscos = asterisco * i
     print(f"{asteriscos}{espacios}")
 
-print("\nPIRAMIDE 2\n")
+print("\nPIRÁMIDE 2\n")
 for i in range(filas+1):
     espacios = espacio * i
     asteriscos = asterisco * (filas-i)
     print(f"{asteriscos}{espacios}")
 
-print("PIRAMIDE 3")
+print("PIRÁMIDE 3")
 for i in range(filas+1):
     espacios = espacio * (filas-i)
     asteriscos = asterisco * ((2*i)-1)
     print(f"{espacios}{asteriscos}")
 
-print("\nPIRAMIDE 4")
+print("\nPIRÁMIDE 4")
 for i in range(filas+1):
     asteriscos = asterisco * i
     espacios = espacio * (filas-i)
