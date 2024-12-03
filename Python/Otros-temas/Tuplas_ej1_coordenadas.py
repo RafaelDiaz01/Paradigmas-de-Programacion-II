7175ea5c738f035aa4b620c2b52ba1728f6ec224
Rafael Díaz desde Laptop <rafaeldiazmx98@gmail.com>
2024-12-02 23:19:26 -0600

    Código comentado adecuadamente y listo para subir a la plataforma educativa.

---
+++ Python/Otros-temas/Tuplas_ej1_coordenadas.py
@@ -0,0 +1,11 @@
+# Kevin Rafael Díaz López - 13/11/2024
+# Programa que muestra el uso de los tuplas (ordenadas e inmutables).
+
+# Códigos para dar color al título.
+GREEN = "\033[32m"  # Define el color verde para el texto.
+RED = "\033[31m" # Define el color rojo para el texto.
+RESET = "\033[0m"  # Define el color de reinicio para volver al color original.
+
+# Título del programa.
+print(f"\n\t{GREEN}|TUPLAS COORDENADAS|{RESET}")  # Imprime el título del programa en color verde.
+print()  # Imprime una línea en blanco.
